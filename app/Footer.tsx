6e65892ffa5991b988c07422c440c7ec69bef39d
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2024-02-02 18:09:23 +0000

    Add contact list in page footer

---
+++ app/Footer.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import styles from "./Footer.module.css"
+export default function Footer() {
+    return (
+        <div className={styles.body}>
+            <FooterLinks></FooterLinks>
+        </div>
+    );
+}
+
+const FooterLinks = () => {
+    return (
+        <ul>
+            <li>
+                <img src="gh-logo.svg"></img>
+                <Link href={"https://github.com/IbrahimAlshubaily"}>https://github.com/IbrahimAlshubaily</Link>
+            </li>
+            <li>
+                <img src="linkedin-logo.svg"></img>
+                <Link href={"https://linkedin.com/in/alshubaily/"}>linkedin.com/in/alshubaily/</Link>
+            </li>
+            <li>
+                <img src="mail-logo.svg"></img>
+                <Link href={"mailto:ibrahim.alshubaily.sde@gmail.com"}>ibrahim.alshubaily.sde@gmail.com</Link>
+            </li>
+        </ul>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavMenue from "./NavMenu";
+import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,8 @@ export default function RootLayout({
         <NavMenue />
         <hr />
         {children}
+        <hr />
+        <Footer />
       </body>
     </html>
   );
